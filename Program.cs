de23d844931c37f9833f9393bfcc2a21dc9ce59d
TheProgrammer <wonderprog17@gmail.com>
2024-05-08 19:00:30 +0600

    Save the program

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿object[] mixedArray = new object[3];
+
+mixedArray[0] = "Hello";
+mixedArray[1] = 2;
+mixedArray[2] = ":-)";
+
+
+List<object> resultArray = new List<object>();
+for (int i = 0; i < mixedArray.Length; i++){
+    if (mixedArray[i].ToString().Length >= 4){
+            resultArray.Add(mixedArray[i]);
+        }
+}
+
+        foreach (object item in resultArray){
+            Console.WriteLine(item);
+        }
